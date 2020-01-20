1dbbe12a3478e07ad9b35a29d92cb23251777847
ladybirds1 <33885117+ladybirds1@users.noreply.github.com>
2020-01-20 19:32:40 +0800

    Add files via upload
    增加engine库进行矩阵运算加速，同时修复int精度带来的bug

--- svm/svm/svm.cpp
+++ svm/svm/svm.cpp
@@ -5,25 +5,33 @@
 #include <stdlib.h>
 #include <math.h>
 #include <vector>
-
+#include <cstdlib>
+#include <ctime>
+#include <Eigen/Dense>
 
 // This is a SVM
 // Basic on Li Hang 《统计学习方法》
 
+typedef Eigen::VectorXf EigenVectorFloat;
+typedef Eigen::MatrixXf EigenMatrixFloat;
+typedef Eigen::VectorXi EigenVectorInt;
+
 // 浮点型向量
-typedef std::vector<float> FLOATVECTOR;
+typedef std::vector<float> VectorFloat;
 // 整型向量
-typedef std::vector<int> INTVECTOR;
+typedef std::vector<int> VectorInt;
 // 浮点型矩阵
-typedef std::vector<FLOATVECTOR> FLOATMAT;
+typedef std::vector<VectorFloat> FloatMat;
 // 整形矩阵
-typedef std::vector<INTVECTOR> INTMAT;
+typedef std::vector<VectorInt> IntMat;
 
 // 从文件中读取数据及分类标签
 // 输入:文件路径, 读取行数
-void LoadDate(const char *filename,int line,
-              FLOATMAT &datamat,INTVECTOR &labellist) {
+void LoadDate(const char *filename, int line, int col,
+             EigenMatrixFloat &datamat, EigenVectorInt &labellist) {
 
+  labellist = EigenVectorInt(line);
+  datamat = EigenMatrixFloat(line, col);
   // 打开文件
   std::ifstream inFile(filename, std::ios::in);
   if (inFile.fail()) {
@@ -33,6 +41,7 @@ void LoadDate(const char *filename,int line,
   std::string lineStr;
   // 从文件中读取一行数据到lineStr中
   int i = 0;
+  int lines = -1;
   while (getline(inFile, lineStr) && line--) {
     // str 用来装逗号隔开的数据
     std::string str;
@@ -40,7 +49,7 @@ void LoadDate(const char *filename,int line,
     std::stringstream ss(lineStr);
     // 设置一个数据读取的标志位
     int start = 0;
-    FLOATVECTOR datalists;
+    int cols = 0;
     // 分割lineStr这行数据逗号隔开的每一个分别存储
     while (getline(ss, str, ',')) {
       // 如果是这行的第一个数字（标签）
@@ -48,21 +57,22 @@ void LoadDate(const char *filename,int line,
         // atoi <stdlib.h> 将字符串中的数据转换为int型
         int label = atoi(str.c_str());
         if (label == 0) {
-          labellist.push_back(1);
+          labellist(i) = 1;
           printf("第%d标签：1\n", ++i);
-        } else {
-          labellist.push_back(-1);
+        }
+        else {
+          labellist(i) = -1;
           printf("第%d标签：-1\n", ++i);
         }
-        
+        // 行数增加
+        ++lines;
         start = 1;
         continue;
       }
-       // 转换到0~1之间的数
-       float data = (float)atoi(str.c_str()) / 255;
-       datalists.push_back(data);
+      float data = atof(str.c_str());
+      datamat(lines, cols) = data;
+      ++cols;
     }
-    datamat.push_back(datalists);
   }
   printf("读入数据行数：%d\n", i);
 }
@@ -77,8 +87,8 @@ public:
   // sigma:高斯核中的分母sigma
   // C:惩罚系数
   // toler:松弛变量
-  SVM(FLOATMAT &datalist, INTVECTOR &labellist, 
-      float sigma, float C, float toler) {
+  SVM(EigenMatrixFloat &datalist, EigenVectorInt &labellist,
+    float sigma, float C, float toler) {
     traindatalist = datalist;
     trainlabellist = labellist;
     if (!datalist.size()) {
@@ -86,18 +96,18 @@ public:
       return;
     }
     // 这一步一定要判空
-    m = datalist.size();
-    n = datalist[0].size();
+    m = datalist.rows();
+    n = datalist.cols();
     sigmas = sigma;
     Cs = C;
     tolers = toler;
     b = 0;
-    
+
     CalcKernal();
     // 给所有的alpha付出值为0，长度为训练集数目
-    alpha = FLOATVECTOR(m, 0);
+    alpha = VectorFloat(m, 0);
     // 给所有的Ei也付初值为0，长度为训练集数目
-    E = FLOATVECTOR(m, 0);
+    E = VectorFloat(m, 0);
     // 支持向量数组
     //supportvecindex = FLOATVECTOR(m, 0);
   }
@@ -109,47 +119,46 @@ public:
     // 初始化高斯核结果矩阵大小 = 训练集的长度的平方
     // 令有m个向量，每个向量里面m个元素
     // kernelanswer[i][j] = xi * xj;
-    kernelanswer = FLOATMAT(m, FLOATVECTOR(m, 0));
-    
+    kernelanswer = FloatMat(m, VectorFloat(m, 0));
+    float result;
+
     // 大循环遍历xi
     for (int i = 0; i < m; ++i) {
       // 向量xi
-      FLOATVECTOR X = traindatalist[i];
+      EigenVectorFloat X = traindatalist.row(i);
       // 小循环遍历，只用计算一半即可
       for (int j = i; j < m; ++j) {
-        FLOATVECTOR Z = traindatalist[j];
+        EigenVectorFloat Z = traindatalist.row(j);
         // ||X-Z||^2
-        float a = 0;
-        for (int s = 0; s < n; ++s) {
-          a += (X[s] - Z[s]) * (X[s] - Z[s]);
-        }
-        //计算出高斯核结果
-        a = exp(-a / (2 * sigmas * sigmas));
-        kernelanswer[i][j] = a;
-        kernelanswer[j][i] = a;
+        result = (X - Z).squaredNorm();
+        result = exp(-1 * result / (2 * sigmas * sigmas));
+        kernelanswer[i][j] = result;
+        kernelanswer[j][i] = result;
       }
     }
-    printf("K784784:%f\n", kernelanswer[999][999]);
   }
- 
+
   // kkt条件满足判断
   // 查看第i个alpha是否满足kkt条件
   // return true,满足，false，不满足
   bool IsSatisfyKKT(int i) {
     // 算出g(x)
     float gxi = CalcGxi(i);
-    int yi = trainlabellist[i];
+    int yi = trainlabellist(i);
+    //printf("i:%d gxi:%f yi:%d\n",i,gxi,yi);
 
     // 根据“7.4.2 变量的选择方法”中“第一个变量的选择”
     // 7.111 到 7.113
-    if ((fabs(alpha[i]) < tolers) && (yi * gxi >= 1)){
+    if ((fabs(alpha[i]) < tolers) && (yi * gxi >= 1)) {
       return true;
-    } else if ((fabs(alpha[i] - Cs) < tolers) && (yi * gxi <= 1)) {
+    }
+    else if ((fabs(alpha[i] - Cs) < tolers) && (yi * gxi <= 1)) {
       return true;
-    } else if ((alpha[i] > -tolers) && (alpha[i] < (Cs + tolers)) && \
-               (fabs(yi * gxi - 1) < tolers)) {
+    }
+    else if ((alpha[i] > -tolers) && (alpha[i] < (Cs + tolers)) && \
+      (fabs(yi * gxi - 1) < tolers)) {
       return true;
-    } 
+    }
     return false;
   }
 
@@ -170,24 +179,29 @@ public:
     // 如果不为零就加上
     for (int j = 0; j < m; ++j) {
       if (alpha[j] != 0) {
-        gxi += alpha[j] * trainlabellist[j] * kernelanswer[j][i];
+        //printf("gxi: %d\n",j);
+        //printf("alpha:%f\n", alpha[j]);
+        //gxi = gxi + alpha[j] * trainlabellist(j) * kernelanswer[j][i];
+        int yj = trainlabellist(j);
+        gxi = gxi + (alpha[j] * yj * kernelanswer[j][i]);
       }
     }
-    gxi += b;
+    gxi = gxi + b;
     return gxi;
   }
 
   // 计算误差
   // 按照式子 7.105
   float CalcEi(int i) {
-    return (CalcGxi(i) - trainlabellist[i]);
+    float gxi = CalcGxi(i);
+    return (gxi - trainlabellist(i));
   }
 
   // 获得第二个alpha
   // 第一个alpha通过不符合KKT条件选取
   // 输入：E1，第一个变量的误差，i,第一个alpha
   // 返回第二个alpha的序号
-  int GetAlphaJ(int E1, int i) {
+  int GetAlphaJ(float E1, int i) {
     int alpha2_index = -1;
     // 第二个变量选取的标准是E1-E2
     float maxE1_E2 = -1;
@@ -201,10 +215,16 @@ public:
         }
       }
     }
-    // 若E数组里面没有非0的，就直接取出i后面这个
-    if (maxE1_E2 == -1) {
-      alpha2_index = (i + 2) % m;
+    // 若E数组里面没有非0的，就随机的在所有中选一个
+    if (alpha2_index == -1) {
+      //srand(int(time(0)));
+      //int r = rand() % m;
+      //while (r > m) {
+      //  r = r % m;
+      //}
+      alpha2_index = i + 2;
     }
+    //printf("the maxindex is: %d\n", alpha2_index);
     return alpha2_index;
   }
 
@@ -219,7 +239,7 @@ public:
     // 如果已经达到迭代次数，或者已经没有参数改变-->认为已经达到收敛状态
     while (iter < count && paramerter_changed > 0) {
       // 打印当前迭代次数
-      printf("iter: %d \n",iter);
+      printf("iter: %d \n", iter);
       iter += 1;
       paramerter_changed = 0;
 
@@ -228,11 +248,12 @@ public:
       for (int k = 0; k < m; ++k) {
         // 如果不满足KKT条件，就选出本次alpha
         if (!IsSatisfyKKT(k)) {
-
+          //printf("K:%d", k);
           // 先计算E1,alpha2newnew = alpha2old + yi(E1 - E2)/(K11 + K22 - 2K12）
           float E1 = CalcEi(k);
+          //printf("i = %d, E1 = %\n", k, E1);
           // 选取第二个alpha
-          int j = GetAlphaJ(E1,k);
+          int j = GetAlphaJ(E1, k);
           float E2 = CalcEi(j);
           // 所以到这里 k 是第一个alpha的下标
           // j 是第二个alpha的下标
@@ -240,10 +261,11 @@ public:
           // 计算alpha2new的L和H
           // 若y1 != y2
           float L = 0, H = 0;
-          if (trainlabellist[k] != trainlabellist[j]) {
+          if (trainlabellist(k) != trainlabellist(j)) {
             L = fmax(0, alpha[j] - alpha[k]);
             H = fmin(Cs, Cs + alpha[j] - alpha[k]);
-          } else {
+          }
+          else {
             L = fmax(0, alpha[k] + alpha[j] - Cs);
             H = fmin(Cs, alpha[k] + alpha[j]);
           }
@@ -257,32 +279,37 @@ public:
           float K21 = kernelanswer[j][k];
           float K22 = kernelanswer[j][j];
 
-          float alpha2new_new = alpha[j] + trainlabellist[j] * (E1 - E2) \
-                                / (K11 + K22 - 2 * K12);
+          float alpha2new_new = alpha[j] + trainlabellist(j)  * (E1 - E2) \
+            / (K11 + K22 - 2 * K12);
           // 剪切alpha2new_new赋值给alpha2
           if (alpha2new_new < L) {
             alpha2new_new = L;
-          } else if (alpha2new_new > H) {
+          }
+          else if (alpha2new_new > H) {
             alpha2new_new = H;
           }
           // 计算alpha1new
           // 遵照式子 7.109
-          float alpha1new = alpha[k] + trainlabellist[k] * trainlabellist[j] \
-                           * (alpha[j] - alpha2new_new);
+          float alpha1new = alpha[k] + trainlabellist(k) * trainlabellist(j) \
+            * (alpha[j] - alpha2new_new);
 
           // 计算b
           // 依据“7.4.2 变量的选择方法”第三步式7.115和7.116计算b1和b2
-          float b1 = -1 * E1 - trainlabellist[k] * K11 * (alpha1new - alpha[k])\
-                     - trainlabellist[j] * K21 * (alpha2new_new - alpha[j]) + b;
-          float b2 = -1 * E2 - trainlabellist[k] * K12 * (alpha1new - alpha[k])\
-                     - trainlabellist[j] * K22 * (alpha2new_new - alpha[j]) + b;
+          float b1 = -1 * E1 - trainlabellist(k) * K11 * (alpha1new - alpha[k])\
+            - trainlabellist(j) * K21 * (alpha2new_new - alpha[j]) + b;
+          //printf("b1: %f\n", b1);
+          float b2 = -1 * E2 - trainlabellist(k) * K12 * (alpha1new - alpha[k])\
+            - trainlabellist(j) * K22 * (alpha2new_new - alpha[j]) + b;
+          //printf("b2: %f\n", b2);
 
           // 依据alpha1和alpha2的值范围确定新的b
           if (alpha1new > 0 && alpha1new < Cs) {
             b = b1;
-          } else if (alpha2new_new > 0 && alpha2new_new < Cs) {
+          }
+          else if (alpha2new_new > 0 && alpha2new_new < Cs) {
             b = b2;
-          } else {
+          }
+          else {
             b = (b1 + b2) / 2;
           }
 
@@ -300,7 +327,7 @@ public:
       }
       printf("迭代次数:%d，改变alpha个数:%d\n", iter, paramerter_changed);
     }
-    printf(" b = %d\n", b);
+    printf(" b = %f\n", b);
     // 保存所有的支持向量的索引
     for (int s = 0; s < m; ++s) {
       if (alpha[s] > 0) {
@@ -311,38 +338,40 @@ public:
 
   // 做预测的时候需要单独的计算核函数
   float PredictCalcSingleKernel(int i, int j) {
-     // 向量xi是训练集中的支持向量,保存支持向量的数组里面
-     FLOATVECTOR X = traindatalist[i];
-     // 测试向量
-     FLOATVECTOR Z = testdatalist[j];
-     // ||X-Z||^2
-     float a = 0;
-     for (int s = 0; s < n; ++s) {
-        a += (X[s] - Z[s]) * (X[s] - Z[s]);
-     }
-     //计算出高斯核结果
-     a = exp(-a / (2 * sigmas * sigmas));
-     return a;
+    // 向量xi是训练集中的支持向量,保存支持向量的数组里面
+    EigenVectorFloat X = traindatalist.row(i);
+    // 测试向量
+    EigenVectorFloat Z = testdatalist.row(j);
+    // ||X-Z||^2
+    float a = 0;
+    /*for (int s = 0; s < n; ++s) {
+       a += (X[s] - Z[s]) * (X[s] - Z[s]);
+    }*/
+    a = (X - Z).squaredNorm();
+    //计算出高斯核结果
+    a = exp(-a / (2 * sigmas * sigmas));
+    return a;
   }
-    
+
   // 预测
   int predict(int i) {
-    float K_p = 0,result = 0;
+    float K_p = 0, result = 0;
     // 遍历所有的支持向量，计算求和公式
     for (int k = 0; k < supportvecindex.size(); ++k) {
       // 先单独的将预测的核函数求出来
       // 传入支持向量的索引和测试向量的索引
       // alpha[k:n] * y[k:n] * K[k:n,i]
-      K_p = PredictCalcSingleKernel(supportvecindex[k],i);
+      K_p = PredictCalcSingleKernel(supportvecindex[k], i);
       // 逐步求和
-      result = result + K_p * trainlabellist[supportvecindex[k]] * alpha[supportvecindex[k]];
+      result = result + K_p * trainlabellist(supportvecindex[k]) * alpha[supportvecindex[k]];
     }
     result += b;
 
     // sign()
     if (result < 0) {
       return (-1);
-    } else {
+    }
+    else {
       return (1);
     }
   }
@@ -351,26 +380,109 @@ public:
   void Test() {
     int error_count = 0;
     // 遍历测试集所有样本
-    for (int i = 0; i < testdatalist.size(); ++i) {
-      //printf("预测结果为： %d\n", predict(i));
+    for (int i = 0; i < testdatalist.rows(); ++i) {
+      printf("预测结果为： %d\n", predict(i));
       int r = predict(i);
-      printf("预测结果为：%d\n", r);
-      if (r != testlabellist[i]){
-        printf("出错索引：%d ，预测标签为：%d ,原来标签为：%d\n", i, r, testlabellist[i]);
+      //printf("预测结果为：%d\n", r);
+      if (r != testlabellist(i)) {
+        printf("出错索引：%d ，预测标签为：%d ,原来标签为：%d\n", i, r, testlabellist(i));
         error_count += 1;
       }
     }
-    printf("出错的个数：%d", error_count);
+    printf("出错的个数：%d\n", error_count);
+    printf("正确率: %f\n", 1 - (float)error_count / (float)testdatalist.rows());
   }
 
-  FLOATVECTOR supportvecindex;  // 支持向量数组
-  FLOATVECTOR alpha;            // 拉格朗日乘子数组
-  FLOATVECTOR E;                // SMO算法运算过程中的E的数组
-  FLOATMAT traindatalist;       // 训练数据集
-  INTVECTOR trainlabellist;     // 标签集
-  FLOATMAT testdatalist;        // 测试数据集
-  INTVECTOR testlabellist;      // 测试标签集
-  FLOATMAT kernelanswer;        // 核函数结果矩阵
+  // 保存模型
+  bool SaveModel() {
+    std::ofstream outFile;
+    outFile.open("\model.csv", std::ios::out | std::ios::trunc);
+    if (outFile.is_open()) {
+      for (int i = 0; i < supportvecindex.size(); ++i) {
+        outFile
+          << trainlabellist(supportvecindex[i]) << ",";
+        for (int j = 0; j < n; ++j) {
+          outFile
+            << traindatalist(supportvecindex[i], j) << ",";
+        }
+        outFile
+          << alpha[supportvecindex[i]] << std::endl;
+      }
+      outFile << b << std::endl;
+      outFile.close();
+      return true;
+    }
+    return false;
+  }
+
+  // 读取模型
+  // 输入：模型路径，支持向量的个数，向量特征值个数
+  void GetModel(const char *filename, int line, int col,
+    EigenMatrixFloat &datamat, EigenVectorInt &labellist) {
+    labellist = EigenVectorInt(line);
+    datamat = EigenMatrixFloat(line, col);
+    // 打开文件
+    std::ifstream inFile(filename, std::ios::in);
+    if (inFile.fail()) {
+      puts("读取文件失败！");
+    }
+    // CSV文件中一行数据的容器
+    std::string lineStr;
+    // 从文件中读取一行数据到lineStr中
+    int i = 0;
+    int lines = -1;
+    while (getline(inFile, lineStr) && line--) {
+      // str 用来装逗号隔开的数据
+      std::string str;
+      // 这一步参考网上的
+      std::stringstream ss(lineStr);
+      // 设置一个数据读取的标志位
+      int start = 0;
+      int cols = 0;
+      // 设置supportvector数组
+      supportvecindex.push_back(i);
+      // 分割lineStr这行数据逗号隔开的每一个分别存储
+      while (getline(ss, str, ',')) {
+        // 如果是这行的第一个数字（标签）
+        if (!start) {
+          // atoi <stdlib.h> 将字符串中的数据转换为int型
+          int label = atoi(str.c_str());
+          if (label == 0) {
+            labellist(i) = 1;
+            printf("第%d标签：1\n", ++i);
+          }
+          else {
+            labellist(i) = -1;
+            printf("第%d标签：-1\n", ++i);
+          }
+          // 行数增加
+          ++lines;
+          start = 1;
+          continue;
+        }
+        if (cols < col) {
+          // 转换到0~1之间的数
+          float data = atof(str.c_str());
+          datamat(lines, cols) = data;
+          ++cols;
+          continue;
+        }
+        alpha[lines] = atof(str.c_str());
+      }
+    }
+    // 设置B
+    b = 0;
+    printf("读入数据行数：%d\n", i);
+  }
+
+  VectorInt supportvecindex;    // 支持向量数组
+  VectorFloat alpha;            // 拉格朗日乘子数组
+  VectorFloat E;                // SMO算法运算过程中的E的数组
+  EigenMatrixFloat traindatalist;       // 训练数据集
+  EigenVectorInt trainlabellist;     // 标签集
+  EigenMatrixFloat testdatalist;        // 测试数据集
+  EigenVectorInt testlabellist;      // 测试标签集
+  FloatMat kernelanswer;        // 核函数结果矩阵
   int m;                        // 数据集长度  
   int n;                        // 样本特征数量
   float b;                      // SVM中的偏置
@@ -383,20 +495,23 @@ private:
 
 void main() {
   // 数据集和标签集矩阵
-  FLOATMAT datalist, testlist;
-  INTVECTOR labellist, testlabellist;
+  EigenMatrixFloat datalist, testlist;
+  EigenVectorInt labellist, testlabellist;
 
   // 从数据集中导入数据和标签
-  LoadDate("C:\\Users\\shuangjiang ou\\Desktop\\Mnist\\mnist_train.csv",1000, datalist,labellist);
+  LoadDate("C:\\Users\\shuangjiang ou\\Desktop\\数据集\\newtrain_data.csv", 311, 100, datalist,labellist);
+  //LoadDate("C:\\Users\\shuangjiang ou\\Desktop\\data_test.csv", 1800, 3, datalist,labellist);
   // 从数据集中导入测试数据和标签
-  LoadDate("C:\\Users\\shuangjiang ou\\Desktop\\Mnist\\mnist_test.csv", 100, testlist, testlabellist);
+  LoadDate("C:\\Users\\shuangjiang ou\\Desktop\\数据集\\newtrain_data.csv", 311, 100, testlist, testlabellist);
+  //LoadDate("C:\\Users\\shuangjiang ou\\Desktop\\data_test.csv", 1800, 3, testlist, testlabellist);
   // LoadDate("C:\\Users\\shuangjiang ou\\Desktop\\Mnist\\mnist_train.csv", 200, testlist, testlabellist);
-  SVM svm(datalist, labellist, 10, 200, 0.001);
+  SVM svm(datalist, labellist, 10, 100, 0.04);
   svm.testdatalist = testlist;
   svm.testlabellist = testlabellist;
   
   svm.Train(1000);
   svm.Test();
+  svm.SaveModel();
   int i;
   scanf("%d", &i);
 
